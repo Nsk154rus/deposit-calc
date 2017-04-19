853dfa67b009014b8328797df9135869b298da1b
Nsk154rus <sardalik-sardalik@mail.ru>
2017-04-19 09:16:47 +0700

    income

--- lab2.c
+++ lab2.c
@@ -5,5 +5,42 @@ int main()
     
  	printf("Введите сумму вклада (в рублях) и его срок (в днях)\n");
  	printf("Максимальный срок вклада - 365 дней\n");
- 	
+ 	scanf("%d %d", &sum, &time);
+	if (sum > 10000 && time > 0 && time < 365) {
+ 		if (sum < 100000) {
+ 			if (time < 31) {
+ 				percent = -10;
+ 			} else {
+ 				if (time < 121) {
+ 					percent = 2;
+ 				} else {
+ 					if (time < 241) {
+ 						percent = 6;
+ 					} else {
+ 						percent = 12;
+ 					}
+ 				}
+ 			}
+ 		} else {
+ 		     if (time < 31) {
+                 percent = -10;
+             } else {
+                 if (time < 121) {
+                     percent = 3;
+                    if (time == 120) {
+                    	printf("Бинго! \n");
+                    }
+                 } else {
+                     if (time < 241) {
+                         percent = 8;
+                     } else {
+                         percent = 15;
+                     }
+                 }
+             }
+ 		}
+	
+ 	} else {
+ 		printf("Данные введены некорректно\n");
+   	}
 }
